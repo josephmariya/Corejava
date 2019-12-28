26dd45c9f7a52efccd2061375dd31d67201a2999
josephmariya <58158506+josephmariya@users.noreply.github.com>
2019-12-28 11:39:14 +0530

    Sample program for abstract class

--- abstractclass1.java
+++ abstractclass1.java
@@ -1,9 +1,9 @@
 package abstraction;
 
-public abstract class abstractclass1 {
+public abstract class abstractclass1 {     //Abstract class
 	
-     abstract void add(int a,int b);
-     public void substract(int a,int b)
+     abstract void add(int a,int b);       //Abstract method
+     public void substract(int a,int b)    //Concrete method
  	{
  		int result=a-b;
  		System.out.println("Difference is :"+result);
