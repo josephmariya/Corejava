a1dc51904d9ea7f5bdcb002f62d3dae99c913fde
josephmariya <58158506+josephmariya@users.noreply.github.com>
2019-12-30 12:52:43 +0530

    Registration using get() and set() methods

--- classobj3b.java
+++ classobj3b.java
@@ -1,70 +1,49 @@
 package corejava;
 
 public class classobj3b {
-public String name,address,ph,email,ptype,pid;
-	
-	
+
+	public String name,address,ph,email,ptype,pid;//declaring data members
+	//set method is used to set values into variables
 	public void setname(String name)
 	{
-		
 		this.name=name;
-		
 	}
 	public void setaddress(String address)
 	{
-		
-		this.address=address;
-		
+		this.address=address;	
 	}
 	public void setph(String ph)
 	{
-		
 		this.ph=ph;
-		
-		
 	}
 	public void setemail(String email)
 	{
-		
 		this.email=email;
-		
 	}
 	public void setptype(String ptype)
 	{
-		
-		this.ptype=ptype;
-		
-		
+		this.ptype=ptype;	
 	}
 	public void setpid(String pid)
 	{
-		
-		this.pid=pid;
-		
+		this.pid=pid;	
 	}
+	//get method is used to get value from variables
 	public String getname()
 	{
-	
-		return this.name;
-		
+		return this.name;	
 	}
 	public String getaddress()
 	{
-	
 		return this.address;
-		
 	}
 	public String getph()
 	{
-	
-		return this.ph;
-		
+		return this.ph;	
 	}
 	public String getemail()
 	{
-	
-		return this.email;
-		
+		return this.email;	
 	}
 	public String getptype()
 	{
@@ -73,7 +52,6 @@ public String name,address,ph,email,ptype,pid;
 	public String getpid()
 	{
 		return this.pid;
-		
 	}
 
 }
