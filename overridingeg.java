3e29668231694f25ad961ade6ad713ac3b3e820c
josephmariya <58158506+josephmariya@users.noreply.github.com>
2019-12-31 18:29:26 +0530

    Program for overriding

--- overridingeg.java
+++ overridingeg.java
@@ -1,22 +1,22 @@
 package corejava;
 import java.util.Scanner;
-class Animal
+class Animal//Base class
 {
-	public void animalsound()
+	public void animalsound() //method overrided
 	{
 		System.out.println("Sounds of different animals");
 	}
 }
-class Lion extends Animal
+class Lion extends Animal//inherited class
 {
-	public void animalsound()
+	public void animalsound()//method overrided
 	{
 		System.out.println("The Loin roars");
 	}
 }
-class Cat extends Animal
+class Cat extends Animal//inherited class
 {
-	public void animalsound()
+	public void animalsound()//method overrided
 	{
 		System.out.println("The Cat meows");
 	}
@@ -25,12 +25,12 @@ public class overridingeg {
 	
 	public static void main(String args[])
 	{
-		Animal a=new Animal();
-		Animal l=new Lion();
-		Animal c=new Cat();
-		a.animalsound();
-		l.animalsound();
-		c.animalsound();
+		Animal a=new Animal();//object for parent class
+		Animal l=new Lion();//Creates object for Animal class invoking Lion class constructor
+		Animal c=new Cat();//Creates object for Animal class invoking Cat class constructor
+		a.animalsound();//invokes parent class method
+		l.animalsound();//invokes lion class method
+		c.animalsound();//invokes Cat class method
 	}
 
 }
