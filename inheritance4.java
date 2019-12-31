493bcdb4f3b67f2a49d8691ac9d9765dad8cf46f
josephmariya <58158506+josephmariya@users.noreply.github.com>
2019-12-31 12:34:39 +0530

    Program for method overriding

--- inheritance4.java
+++ inheritance4.java
@@ -1,16 +1,16 @@
 package corejava;
 import java.util.Scanner;
-class A
+class A //Base class
 {
-	public void add(int a,int b)
+	public void add(int a,int b) //Method overriding
 	{
 		int n=a+b;
 		System.out.println("Sum is : "+n);
 	}
 }
-public class  inheritance4 extends A{
+public class  inheritance4 extends A{  //Child class
 	int a,b;
-	public void add(int a,int b)
+	public void add(int a,int b) //Method overriding
 	{
 		int n=a+b+10;
 		System.out.println("Sum is : "+n);
@@ -22,8 +22,8 @@ public class  inheritance4 extends A{
 		int a=obj.nextInt();
 		System.out.println("Enter second number: ");
 		int b=obj.nextInt();
-		A ob=new inheritance4();
-		ob.add(a,b);
+		A ob=new inheritance4();//Creating object for parent class by invoking child class constructor
+		ob.add(a,b);//it checks add() method in child class first then looks it present in base class if it is present then load the child class add() method
 	}
 	
 	
