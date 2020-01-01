085d528a6997df15cf913b4b0d20f2dde3949daf
josephmariya <58158506+josephmariya@users.noreply.github.com>
2020-01-01 15:23:22 +0530

    Program for interface

--- interfacea1.java
+++ interfacea1.java
@@ -1,7 +1,7 @@
 package interfacesample;
 
-public interface interfacea1 {
-	public abstract void add(int a,int b);
+public interface interfacea1 { //Declaring interface
+	public abstract void add(int a,int b); //Declaring abstract methods
 	public abstract void sub(int a,int b);
 
 }
