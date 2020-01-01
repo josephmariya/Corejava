5fc54e041b70ca3abf2592b70381669845301c85
josephmariya <58158506+josephmariya@users.noreply.github.com>
2020-01-01 15:24:47 +0530

    Program for interface

--- interfacea2.java
+++ interfacea2.java
@@ -1,7 +1,7 @@
 package interfacesample;
 
-public interface interfacea2 {
+public interface interfacea2 { //Declaring interface
   
-	public void mul(int a,int b);
+	public void mul(int a,int b); //Declaring abstract methods
 	public void div(int a,int b);
 }
